b33e0ae255f13bc219757f3f7d044043215527e3
root <root@d12.homeland.local>
2024-09-15 20:19:22 +0300

    feat: finish part one

--- main.sh
+++ main.sh
@@ -1,5 +1,10 @@
 interface=enp0s3
 
+staticIp="10.100.0.2"
+mask="255.255.255.0"
+gateway="10.100.0.1"
+dns="8.8.8.8"
+
 function networkInfo() {
 	echo "Network card model and vendor:"
 	lshw -C network | grep "vendor"
@@ -30,7 +35,46 @@ function currentIpConfig() {
 }
 
 function configureStaticIp() {
-
+	ip addr flush dev $interface
+	ip addr add $staticIp/$mask dev $interface
+	ip route add default via $gateway
+	echo "nameserver $dns" | tee /etc/resolv.conf > /dev/null
+	echo "static ip configuration is done"
 }
 
-currentIpConfig
+function configureDhcp() {
+	dhclient -r $interface
+	dhclient $interface
+	echo "dynamic ip configuration has been applied"
+}
+
+while true; do
+	echo "1) Display network card information"
+	echo "2) Display current IP configuration"
+	echo "3) Configure static IP"
+	echo "4) Configure dynamic IP (DHCP)"
+	echo "5) Exit"
+	read -p "Choose an option: " choice
+
+	case $choice in
+		1) 
+			networkInfo
+			;;
+		2) 
+			currentIpConfig
+			;;
+		3)	
+			configureStaticIp
+			;;
+		4) 
+			configureDhcp
+			;;
+		5) 
+			echo "Exiting script."
+			exit 0
+			;;
+		*)
+			echo "Invalid choice, try again"
+			;;
+	esac
+done
